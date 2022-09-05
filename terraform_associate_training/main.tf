666c507b1986adc48f887df3957221aed2d511e8
navdeep.dhamu <ndhamu@worth.systems>
2022-09-05 09:30:10 +0100

    updated private and public indexes

--- terraform_associate_training/main.tf
+++ terraform_associate_training/main.tf
@@ -19,11 +19,10 @@ resource "aws_vpc" "vpc" {
 
 #Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
-  for_each   = var.private_subnets
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value)
-  availability_zone = tolist(data.aws_availability_zones.available.names)[
-  each.value]
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
 
   tags = {
     Name      = each.key
@@ -33,11 +32,10 @@ resource "aws_subnet" "private_subnets" {
 
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
-  for_each   = var.public_subnets
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone = tolist(data.aws_availability_zones.available.
-  names)[each.value]
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
   map_public_ip_on_launch = true
 
   tags = {
